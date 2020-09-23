154f8bb7f438ae6db5d56faf9fab3e7629dc21c1
mysaltern <mysaltern@gmail.com>
2020-09-23 09:02:32 +0330

    Update db.php

--- config/db.php
+++ config/db.php
@@ -8,6 +8,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=avinManage',
     'username' => 'pmauser',
-    'password' => 'momibs450Asada2@',
+    'password' => '',
     'charset' => 'utf8',
 ];
